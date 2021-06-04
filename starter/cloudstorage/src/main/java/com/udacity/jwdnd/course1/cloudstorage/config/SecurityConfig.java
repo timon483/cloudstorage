b3a5bcc861dd387be1534ff9d02bd0b4d13aeb6b
Timofey Kasimtsev <s2tikas@uni-trier.de>
2021-06-04 11:35:24 +0200

    Classes for controllers, services, data objects and configuration created.

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
