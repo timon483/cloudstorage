accca63e130660bbb419833a1b9cadc639a00758
Timofey Kasimtsev <s2tikas@uni-trier.de>
2021-06-04 12:11:22 +0200

    AuthentificationService, SecurityConfig and UserMapper defined.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -1,4 +1,20 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
+import com.udacity.jwdnd.course1.cloudstorage.data.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
 public interface UserMapper {
+
+
+    @Insert("INSERT INTO USERS(username, salt, password, firstname, lastname) VALUES (#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
+    @Options(useGeneratedKeys = true, keyProperty = "userid")
+    Integer insert(User user);
+
+
+    @Select("SELECT * FROM USERS WHERE username = #{username)")
+    User getUser(String username);
+
+
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthentificationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthentificationService.java
@@ -1,4 +1,49 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
-public class AuthentificationService {
+import com.udacity.jwdnd.course1.cloudstorage.data.User;
+import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class AuthentificationService implements AuthenticationProvider {
+    private HashService hashService;
+    private UserMapper userMapper;
+
+    public AuthentificationService(HashService hashService, UserMapper userMapper){
+        this.hashService = hashService;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException{
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.getUser(username);
+
+        if (user != null){
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if (hashedPassword.equals(user.getPassword())){
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+
+
 }
