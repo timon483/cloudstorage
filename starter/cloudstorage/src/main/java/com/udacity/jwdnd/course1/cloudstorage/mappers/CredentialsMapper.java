e77c0d45b3d3bea03e05693b0bddb2e46b20983d
Timofey Kasimtsev <timon483@gmail.com>
2021-07-02 09:50:57 +0200

    CredentialsMapper with Insert, Select, Update and Delete methods.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
@@ -1,7 +1,23 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.udacity.jwdnd.course1.cloudstorage.data.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface CredentialsMapper {
+
+    @Insert("INSERT INTO CREDENTIALS(url, username, key, password, userid)  VALUES (#{url}, #{username}, #{key}, #{password} ,#{userid})")
+    Integer insert(Credential credential);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE userid = #{userid}")
+    List<Credential> getCredentialsByUser(String userid);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, password = #{password} where credentialid = #{credentialid}")
+    Integer update(Credential credential);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialid}")
+    Integer delete(Integer credentialid);
+
 }
